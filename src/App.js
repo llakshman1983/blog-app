f5da15439f809e3439612a2954e73c0e480bb0ea
llakshman1983 <lakshmanakumar.l@gmail.com>
2021-01-08 01:32:24 -0500

    blog app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Blog from './containers/Blog/Blog';
+import {BrowserRouter} from  'react-router-dom';
+import  './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+      <div className="App">
+        <Blog />
+      </div>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
+
